aa134a25862cd2bd07106df7f1fc8e4c4de58c9f
Alireza Mohammadhossein <mh.alireza1993@gmail.com>
2023-04-17 14:26:03 +0200

    add copy website

--- src/components/services/components/site-copying/SiteCopyingBlock.js
+++ src/components/services/components/site-copying/SiteCopyingBlock.js
@@ -1,22 +1,38 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import CloudBlock from '../../../common/cloud-block/CloudBlock';
 import CopySites from '../../../copy-sites/CopySites';
-import icon from '../../../../assets/images/services/copy-sites-widget.png'
+import icon from '../../../../assets/images/services/copy-sites-widget.png';
 
 
-const SiteCopyingBlock = ({ history }) => {
+const LogoBuilder = () => {
+  const { t } = useTranslation();
   const navigate = useNavigate();
+  
   const content = (
-    <div className="services__sites-copying_container">
-      <input className="site-input" placeholder='Enter the site address...' />
-      <div className="btn services__sites-copying_btn" onClick={() => navigate('/services/copy-sites')}>Copy</div>
+    <div className='services__sites-copying'>
+      <div className="services__sites-copying_container">
+        <div className='services__sites-copying_description'>
+          <p>{t('SERVICES.SITES_COPYING.DESCRIPTON')}</p>
+        </div>
+        <div className='services__sites-copying_form-wrapper'>
+          <p className='services__sites-copying_form_title'>{t('SERVICES.SITES_COPYING.LOGO_NAME')}</p>
+
+          <input className="services__sites-copying_form_input" placeholder={t('SERVICES.SITES_COPYING.PLACEHOLDER')} />
+          
+          <div className='services__sites-copying_form_btn'>
+            <button className="btn" onClick={() => navigate('/services/copy-sites')}>{t('SERVICES.SITES_COPYING.BUTTON')}</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 
   return (
     <CloudBlock
-      title='Copy sites'
+      title={t('SERVICES.SITES_COPYING.TITLE')}
+      subtitle={t('SERVICES.SITES_COPYING.SUBTITLE')}
       rightButtonAction={() => navigate('/services/copy-sites')}
       content={content}
       directComponent={<CopySites />}
@@ -26,4 +42,4 @@ const SiteCopyingBlock = ({ history }) => {
   );
 };
 
-export default SiteCopyingBlock;
+export default LogoBuilder;
