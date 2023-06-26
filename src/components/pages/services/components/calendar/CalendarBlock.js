b77fed81f47fb963dcfe99f8bdbe0463db1e54f5
Alireza <mh.alireza1993@gmail.com>
2023-06-26 12:29:47 +0200

    fixed height of widgets and header popup,, remove background of widgets, take to front left menu

--- src/components/pages/services/components/calendar/CalendarBlock.js
+++ src/components/pages/services/components/calendar/CalendarBlock.js
@@ -6,11 +6,24 @@ import CloudBlock from "../cloud-block/CloudBlock";
 import { getLangISOFormat } from "../../../../../types";
 import CalendarPage from "../../../calendar/CalendarPage";
 import icon from "../../../../../assets/images/my-services/calendar.png";
+import { useSelector, useDispatch } from "react-redux";
+import { handleOpenAppsModal } from '../../../../../redux/app/appsModalSlice';
+
+
+
 
 const CalendarBlock = () => {
   const { t, i18n } = useTranslation();
+  const dispatch = useDispatch();
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
+
+
+  const openAppsModalHandler = (component) => {
+    dispatch(handleOpenAppsModal(component))
+  };
+
+
   return (
     <CloudBlock
       title={t("SERVICES.CALENDAR.TITLE")}
@@ -23,7 +36,8 @@ const CalendarBlock = () => {
           locale={getLangISOFormat(i18n.language)}
           minDetail="month"
           value={new Date()}
-          onChange={() => navigate("/services/calendar")}
+          // onChange={() => navigate("/services/calendar")}
+          onChange={() => openAppsModalHandler(CalendarPage)}
         />
       }
       // mdiIcon="calendar_month"
