7b538c804eb32282e0c96b4733fcb326d22aa98c
Alireza <mh.alireza1993@gmail.com>
2023-09-20 16:47:18 +0200

    congif new server

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API_BASE_URL = "http://localhost:5000";
-const API_BASE_URL = "https://57.128.35.216:3000";
+const API_BASE_URL = "https://51.91.151.225/";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
